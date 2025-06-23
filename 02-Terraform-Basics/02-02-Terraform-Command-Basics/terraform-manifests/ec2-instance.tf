a3a6afbc843fa24464385586bb1213c2d5034658
aranighosal <63284444+aranighosal@users.noreply.github.com>
2025-06-23 22:29:46 +0530

    Update ec2-instance.tf
    corrected

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -16,6 +16,6 @@ provider "aws" {
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
-  instance_type = "t2.micro"
+  ami           = "ami-0c1ac8a41498c1a9c" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t3.micro"
 }
